1e967a7f7ca52d4efba68687d46591c4deb2513d
MoisesSanchez2020 <san19039@byui.edu>
2023-10-23 00:27:53 -0700

    week-1

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -33,13 +33,19 @@ public static class ArraysTester {
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     private static double[] MultiplesOf(double number, int length)
-    {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-
-        return new double[0]; // replace this return statement with your own
+    { // Initialize an empty array to store the multiples.
+        double[] multiples = new double[length];
+        
+        // Loop from 1 to the desired number of multiples.
+        for(int i = 1; i <= length; i++)
+        {
+            // In each iteration, multiply the starting number by the loop counter 
+            // and store it in the array.
+            multiples[i-1] = number * i;
+        }
+        
+        // Return the array.
+        return multiples;
     }
     
     /// <summary>
@@ -52,10 +58,18 @@ public static class ArraysTester {
     /// </summary>
     private static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+         // Determine the pivot point where the list needs to be split.
+        int pivot = data.Count - (amount % data.Count);
 
+        // Slice the list into two parts: before the pivot and after the pivot.
+        List<int> firstPart = data.GetRange(pivot, data.Count - pivot);
+        List<int> secondPart = data.GetRange(0, pivot);
+
+        // Concatenate the two sliced lists in reverse order.
+        firstPart.AddRange(secondPart);
+
+        // Update the original list with the concatenated list.
+        data.Clear();
+        data.AddRange(firstPart);
     }
 }
