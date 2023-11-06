e9b94537cb9a4335c021160aa9f5522b8e3f6d70
Moises R Sanchez <59586957+MoisesSanchez2020@users.noreply.github.com>
2023-11-05 23:46:38 -0800

    Homework W02 Code

--- week02/code/Priority.cs
+++ week02/code/Priority.cs
@@ -8,6 +8,7 @@
  * GitHub repository, unshared Google Drive folder) is acceptable.
  *
  */
+
 public static class Priority {
     public static void Test() {
         // TODO Problem 2 - Write and run test cases and fix the code to match requirements
@@ -17,23 +18,40 @@ public static class Priority {
 
         // Test Cases
 
-        // Test 1
-        // Scenario: 
-        // Expected Result: 
-        Console.WriteLine("Test 1");
+        // Test 1: Enqueue items with varying priorities and ensure the highest is dequeued first
+        priorityQueue.Enqueue("Item1", 1);
+        priorityQueue.Enqueue("Item2", 2);
+        priorityQueue.Enqueue("Item3", 3);
+        var dequeuedItem = priorityQueue.Dequeue();
+        Console.WriteLine(dequeuedItem == "Item3" ? "Passed" : $"Failed: Expected 'Item3', got '{dequeuedItem}'");
 
         // Defect(s) Found: 
 
         Console.WriteLine("---------");
 
-        // Test 2
-        // Scenario: 
-        // Expected Result: 
-        Console.WriteLine("Test 2");
+        // Test 2: Enqueue items with the same priority and ensure the FIFO order is preserved
+        priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue("ItemA", 1);
+        priorityQueue.Enqueue("ItemB", 1);
+        priorityQueue.Enqueue("ItemC", 1);
+        dequeuedItem = priorityQueue.Dequeue();
+        Console.WriteLine(dequeuedItem == "ItemA" ? "Passed" : $"Failed: Expected 'ItemA', got '{dequeuedItem}'");
 
         // Defect(s) Found: 
 
         Console.WriteLine("---------");
+
+        // Test 3: Dequeue from an empty queue and expect an error
+        priorityQueue = new PriorityQueue();
+        try {
+            dequeuedItem = priorityQueue.Dequeue();
+            Console.WriteLine("Failed: Expected an error, but got an item.");
+        }
+        catch (InvalidOperationException ex) {
+            Console.WriteLine("Passed: Caught expected error for empty queue.");
+        }
+
+        // Defect(s) Found: 
 
         // Add more Test Cases As Needed Below
     }
